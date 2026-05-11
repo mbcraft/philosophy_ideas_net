cbd3bd66561d6c53d1e0d92a3f1421e13f9acb5e
Marco Bagnaresi <info@mbcraft.it>
2026-05-11 20:10:15 +0200

    Index filesù

--- src/content/index/it.php
+++ src/content/index/it.php
@@ -45,7 +45,7 @@ require_once (DIR_PREFIX."include/site_variables.php");
 </div>
     <br />
     <div align="center">
-    <h4>Ultimo aggiornamento : 13 Aprile 2026</h4>
+    <h4>Ultimo aggiornamento : 11 Maggio 2026</h4>
     </div>
     <br />
     <br />
