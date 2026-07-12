2517f5fc0561e654112a84a7c7df20862f75245b
Marco Bagnaresi <info@mbcraft.it>
2026-07-12 11:32:32 +0200

    Aggiunto elemento A_665

--- src/include/philosophy_page_support_functions.php
+++ src/include/philosophy_page_support_functions.php
@@ -1,7 +1,7 @@
 <?php
 
 $range_H_A = array('H',1,12,'black');
-$range_A_A = array('A',1,665,'black');
+$range_A_A = array('A',1,666,'black');
 $range_B_A = array('B',0,18,'black');
 $range_C_A = array('C',1,12,'black');
 $range_D_A = array('D',1,3,'black');
