b34ce837742ac148eabaaa3f9bad7e3b9e710404
Marco Bagnaresi <info@mbcraft.it>
2026-08-13 18:45:08 +0200

    Fixed thanks

--- src/content/philosophy/elements/en/T_002.php
+++ src/content/philosophy/elements/en/T_002.php
@@ -5,8 +5,6 @@ T_002 - I wanted to expressly thank:
 	<li>All the people on social networks for the pleasant or interesting content they have shared</li>
 	<li>Some teenagers I have had the pleasure of playing online with</li>
 	<li>Marco Mazzotti, who inspired me with his extreme choice</li>
-	<li>Abigail Ratchford and another person who claimed to be, who I only dealt with remotely</li>
-	<li>Leanne Crow and Jordan Carver for doing the models</li>
 	<li>The trees in the garden and my mother's and my potted plants</li>
 	<li>Who I have not included in this list and who contributed in some way (neighboring people, companies that created quality products or services that I used ...)</li>
 </ul>
